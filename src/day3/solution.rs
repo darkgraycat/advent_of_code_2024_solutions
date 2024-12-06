c5dcd7b12389fc34058d38bf9d2cbcca81da97c4
darkgraycat <jburyanov@gmail.com>
2024-12-06 23:42:46 +0200

    : dont with day3-1

--- src/day3/solution.rs
+++ src/day3/solution.rs
@@ -1,1 +1,29 @@
-pub fn task1(input: String) {}
+use regex::Regex;
+
+pub fn task1(input: String) {
+    let re = Regex::new(r"mul\(\d+,\d+\)").expect("Regex error");
+    let parsed = parse(input, &re);
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").expect("Regex error");
+    let result: i32 = parsed
+        .iter()
+        .filter_map(|instruction| extract(instruction.as_str(), &re))
+        .map(|(a, b)| a * b)
+        .sum();
+
+    println!("{:?}", result);
+}
+
+fn parse(input: String, re: &Regex) -> Vec<String> {
+    re.captures_iter(input.as_str())
+        .map(|cap| cap[0].to_string())
+        .collect()
+}
+
+fn extract(instruction: &str, re: &Regex) -> Option<(i32, i32)> {
+    re.captures(instruction).and_then(|captures| {
+        let num1 = captures[1].parse().ok()?;
+        let num2 = captures[2].parse().ok()?;
+        Some((num1, num2))
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod day2;
 mod day3;
 
 fn main() {
-    let input = fs::read_to_string("src/day2/input.txt").expect("Cannot read file");
-    day2::solution::task2(input);
+    let input = fs::read_to_string("src/day3/input.txt").expect("Cannot read file");
+    day3::solution::task1(input);
 }
