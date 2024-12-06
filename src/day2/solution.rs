60d2c0249fcc2274b2bbd1dd7d0e215e8dd70766
darkgraycat <jburyanov@gmail.com>
2024-12-06 22:50:21 +0200

    : use Option enum

--- src/day2/solution.rs
+++ src/day2/solution.rs
@@ -5,29 +5,37 @@ pub fn task1(input: String) {
     let mut num_of_safe_reports: u32 = 0;
 
     for level in levels.iter() {
-        let is_unsafe_change = check_level(&level, max_safe_difference);
-        if is_unsafe_change {
-            println!("{:?} is SAFE!", level);
-            num_of_safe_reports += 1;
-        } else {
-            println!("{:?} is UNSAFE!", level);
+        let invalid_change = check_level(&level, max_safe_difference);
+        match invalid_change {
+            Some(idx) => println!("{:?} - UNSAFE at {}", level, idx),
+            None => {
+                println!("{:?} - SAFE", level);
+                num_of_safe_reports += 1;
+            }
         }
     }
 
     println!("Result {}", num_of_safe_reports);
 }
 
-fn check_level(level: &Vec<u32>, max_difference: i32) -> bool {
+fn check_level(level: &Vec<u32>, max_difference: i32) -> Option<usize> {
     let is_increasing = level.first() < level.last();
-    level.windows(2).all(|pair| {
+    for (i, pair) in level.windows(2).enumerate() {
         let diff = pair[1] as i32 - pair[0] as i32;
-        if diff == 0 {
-            return false;
+
+        if diff == 0 || diff.abs() > max_difference {
+            return Some(i + 1);
         }
-        let safe_diff = diff.abs() <= max_difference;
-        let safe_dir = if is_increasing { diff > 0 } else { diff < 0 };
-        return safe_diff && safe_dir;
-    })
+
+        let is_invalid_increasing = is_increasing && diff < 0;
+        let is_invalid_decreasing = !is_increasing && diff > 0;
+
+        if is_invalid_increasing || is_invalid_decreasing {
+            return Some(i + 1);
+        }
+    }
+
+    return None;
 }
 
 fn parse(input: String) -> Vec<Vec<u32>> {
