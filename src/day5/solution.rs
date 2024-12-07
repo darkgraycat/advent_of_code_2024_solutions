49461e19c8a5a42bf8cc2595eb2369d6d636d15f
darkgraycat <jburyanov@gmail.com>
2024-12-08 00:44:50 +0200

    : no need to check previos

--- src/day5/solution.rs
+++ src/day5/solution.rs
@@ -2,22 +2,19 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 struct InputData {
-    before: HashMap<u32, Vec<u32>>,
-    after: HashMap<u32, Vec<u32>>,
+    order: HashMap<u32, Vec<u32>>,
     updates: Vec<Vec<u32>>,
 }
 impl InputData {
     fn parse(input: &String) -> Self {
-        let mut before = HashMap::new();
-        let mut after = HashMap::new();
+        let mut order = HashMap::new();
         let (order_list, updates_list) = input.split_once("\n\n").expect("Invalid input");
         order_list.lines().for_each(|line| {
             let (bef, aft) = line.split_once("|").expect("Cannot split by |");
             let bef = bef.parse::<u32>().expect("Cannot parse before value");
             let aft = aft.parse::<u32>().expect("Cannot parse after value");
 
-            before.entry(bef).or_insert(Vec::new()).push(aft);
-            after.entry(aft).or_insert(Vec::new()).push(bef);
+            order.entry(bef).or_insert(Vec::new()).push(aft);
         });
 
         let updates = updates_list
@@ -30,8 +27,7 @@ impl InputData {
             .collect::<Vec<Vec<u32>>>();
 
         InputData {
-            before,
-            after,
+            order,
             updates,
         }
     }
@@ -60,21 +56,10 @@ impl InputData {
             .collect()
     }
 
-    fn check_order(&self, before: u32, after: u32) -> bool {
-        self.is_allowed_after(before, after) && self.is_allowed_before(after, before)
-    }
-
-    fn is_allowed_after(&self, value: u32, after: u32) -> bool {
-        self.before
+    fn check_order(&self, value: u32, after: u32) -> bool {
+        self.order
             .get(&value)
             .map(|befores| befores.contains(&after))
-            .unwrap_or(false)
-    }
-
-    fn is_allowed_before(&self, value: u32, before: u32) -> bool {
-        self.after
-            .get(&value)
-            .map(|afters| afters.contains(&before))
             .unwrap_or(false)
     }
 }
@@ -95,7 +80,31 @@ pub fn task1(input: String) {
 pub fn task2(input: String) {
     let input_data = InputData::parse(&input);
 
+    println!("{:?}", input_data);
+
     let incorrect_updates = input_data.get_incorrect_updates();
 
-    println!("{:?}", incorrect_updates);
+    // 75,97,47,61,53 becomes 97,75,47,61,53.
+    // 61,13,29 becomes 61,29,13.
+    // 97,13,75,29,47 becomes 97,75,47,29,13.
+    // 97,75,29,13      13
+
+    let fixed_updated = incorrect_updates
+        .iter()
+        .map(|update| {
+            let mut current: usize = 0;
+            while current < update.len() {
+                println!("{}", update[current]);
+                current += 1;
+            }
+            return update;
+        })
+        .cloned()
+        .collect::<Vec<Vec<u32>>>();
+
+    println!("{:?}", fixed_updated);
+}
+
+fn find_intersection(vec1: &Vec<u32>, vec2: &Vec<u32>) -> Vec<u32> {
+    vec1.iter().filter(|x| vec2.contains(x)).cloned().collect()
 }
