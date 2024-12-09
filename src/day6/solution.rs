d20ecdbb6379e1a1a8cd0ad89a5ddca068b73c6d
darkgraycat <jburyanov@gmail.com>
2024-12-09 23:11:16 +0200

    : more nicings

--- src/day6/solution.rs
+++ src/day6/solution.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashSet, fmt::Display};
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Direction {
@@ -23,24 +23,107 @@ impl TryFrom<char> for Direction {
 
 #[derive(Debug)]
 struct Guard {
-    position: (usize, usize),
+    position: (i32, i32),
     direction: Direction,
+    visited: HashSet<(i32, i32)>,
+}
+
+impl Guard {
+    fn new(position: (i32, i32), direction: Direction) -> Self {
+        Guard {
+            position,
+            direction,
+            visited: HashSet::new(),
+        }
+    }
+    fn make_step(&mut self, map_grid: &MapGrid) -> bool {
+        let (nx, ny) = self.get_next_step();
+        if !map_grid.is_in_bounds((nx, ny)) {
+            return false;
+        }
+        if map_grid.is_obstacle((nx, ny)) {
+            self.rotate_right();
+            let (nx, ny) = self.get_next_step();
+            self.position = (nx, ny);
+            return true;
+        }
+        self.position = (nx, ny);
+        true
+    }
+
+    fn get_next_step(&self) -> (i32, i32) {
+        let (x, y) = self.position;
+        let (dx, dy) = match self.direction {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+        };
+        (x + dx, y + dy)
+    }
+
+    fn rotate_right(&mut self) {
+        match self.direction {
+            Direction::Up => self.direction = Direction::Right,
+            Direction::Right => self.direction = Direction::Down,
+            Direction::Down => self.direction = Direction::Left,
+            Direction::Left => self.direction = Direction::Up,
+        };
+    }
+}
+
+#[derive(Debug)]
+struct MapGrid {
+    grid: Vec<Vec<bool>>,
+}
+
+impl MapGrid {
+    fn new(grid: Vec<Vec<bool>>) -> Self {
+        MapGrid { grid }
+    }
+
+    fn is_in_bounds(&self, (x, y): (i32, i32)) -> bool {
+        y >= 0 && y < self.grid.len() as i32 && x >= 0 && x < self.grid[0].len() as i32
+    }
+
+    fn is_obstacle(&self, (x, y): (i32, i32)) -> bool {
+        self.grid
+            .get(y as usize)
+            .and_then(|row| row.get(x as usize))
+            .map_or(false, |&cell| cell)
+    }
 }
 
 pub fn task1(input: String) {
     let (grid, (y, x), direction) = parse(&input);
 
-    let guard = Guard{
-        position: (x, y),
-        direction: direction.try_into().expect("No diretion")
-    };
+    let mut guard = Guard::new((x, y), direction.try_into().expect("No direction"));
 
-    println!("{grid:?}\n{guard:?}");
+    let map_grid = MapGrid::new(
+        grid.into_iter()
+            .map(|line| line.into_iter().map(|ch| ch == '#').collect())
+            .collect(),
+    );
+
+    let mut steps = 0;
+    loop {
+        println!("{guard:?} {steps}");
+        let can_move_forward = guard.make_step(&map_grid);
+        steps += 1;
+        if !can_move_forward {
+            break;
+        }
+    }
+
+    println!("{map_grid:?}");
+    println!("{guard:?}");
+
+    println!("Result {}", steps);
 }
 
 pub fn task2(input: String) {}
 
-fn parse(input: &String) -> (Vec<Vec<char>>, (usize, usize), char) {
+fn parse(input: &String) -> (Vec<Vec<char>>, (i32, i32), char) {
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
     let guard = grid
@@ -52,5 +135,5 @@ fn parse(input: &String) -> (Vec<Vec<char>>, (usize, usize), char) {
                 .map(|col| (row, col, line[col]))
         })
         .expect("No guard found");
-    (grid, (guard.0, guard.1), guard.2)
+    (grid, (guard.0 as i32, guard.1 as i32), guard.2)
 }
