c6659190999a7f77aceec35cf59eaf976a5fe8da
darkgraycat <jburyanov@gmail.com>
2024-12-16 21:42:46 +0200

    : small cleanup before fixing

--- src/day6take2/solution.rs
+++ src/day6take2/solution.rs
@@ -6,7 +6,13 @@
 * https://www.reddit.com/r/adventofcode/comments/1hb9odk/2024_day_6_part_2_one_extra_solution_but_it_seems/
 */
 
-use std::{collections::HashSet, fmt::Display, ops::Range};
+use std::{collections::HashSet, ops::Range};
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Direction
+////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 enum Direction {
@@ -27,6 +33,26 @@ impl Direction {
     }
 }
 
+impl TryFrom<char> for Direction {
+    type Error = String;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '^' => Ok(Direction::Up),
+            'v' => Ok(Direction::Down),
+            '<' => Ok(Direction::Left),
+            '>' => Ok(Direction::Right),
+            _ => Err("Cannot parse direction".to_owned()),
+        }
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Position
+////////////////////////////////////////////////////////////////////////////////
+
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Position {
     x: usize,
@@ -38,7 +64,7 @@ impl Position {
         Self { x, y }
     }
 
-    fn move_to(&self, direction: Direction) -> Position {
+    fn moved(&self, direction: Direction) -> Position {
         match direction {
             Direction::Up => Position::new(self.x, self.y - 1),
             Direction::Right => Position::new(self.x + 1, self.y),
@@ -48,6 +74,12 @@ impl Position {
     }
 }
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Guard
+////////////////////////////////////////////////////////////////////////////////
+
 #[derive(Debug, Clone, Copy)]
 struct Guard {
     position: Position,
@@ -56,7 +88,7 @@ struct Guard {
 
 impl Guard {
     fn get_next(&self) -> Position {
-        self.position.move_to(self.direction)
+        self.position.moved(self.direction)
     }
     fn turn(&mut self) {
         self.direction = self.direction.turn_right();
@@ -65,6 +97,12 @@ impl Guard {
         self.position = self.get_next();
     }
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// State
+////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Clone)]
 struct State {
@@ -78,41 +116,24 @@ struct State {
 impl State {
     fn is_looping(&self) -> bool {
         self.visited.contains(&(
-            self.guard.position.move_to(self.guard.direction),
+            self.guard.position.moved(self.guard.direction),
             self.guard.direction,
         ))
     }
 
-    fn would_loop(&self, pos: &Position, dir: &Direction) -> bool {
-        self.visited.contains(&(*pos, *dir))
-    }
-
     fn make_step(&mut self) -> Option<Position> {
         let next_position = self.get_next_position()?;
-        // println!("{:?}", self.visited);
 
         if self.blocks.contains(&next_position) {
             if !self.visited.insert((next_position, self.guard.direction)) {
-                // println!("LOOOOOOooooooOOOOoooP");
                 return None;
             }
             self.guard.turn();
         } else {
-            self.guard.position = next_position;
+            self.guard.forward();
         }
 
         Some(self.guard.position)
-
-        // self.visited
-        //     .insert((self.guard.position, self.guard.direction));
-
-        // if self.blocks.contains(&next_position) {
-        //     self.guard.turn();
-        // } else {
-        //     self.guard.forward();
-        // }
-
-        // Some(self.guard.position)
     }
 
     fn get_next_position(&self) -> Option<Position> {
@@ -123,15 +144,7 @@ impl State {
     fn with_block(&self, position: Position) -> State {
         let mut cloned = self.clone();
         cloned.blocks.insert(position);
-        cloned.visited.clear();
         cloned
-    }
-
-    fn print_visited(&self) -> String {
-        self.visited
-            .iter()
-            .map(|(pos, dir)| format!("{}:{} - {:?}, ", pos.x, pos.y, dir))
-            .collect()
     }
 }
 
@@ -139,27 +152,27 @@ impl TryFrom<String> for State {
     type Error = String;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
+        let guard_idx = value
+            .find(|c| "<>v^".contains(c))
+            .ok_or("No Guard found!")?;
+        let scanline = value.lines().next().ok_or("Grid cannot be oneline")?.len() + 1;
+
+        let guard = Guard {
+            position: Position::new(guard_idx % scanline, guard_idx / scanline),
+            direction: value
+                .chars()
+                .nth(guard_idx)
+                .ok_or("Cannot get direction")?
+                .try_into()?,
+        };
+
         let blocks: HashSet<Position> = value
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.chars()
-                    .enumerate()
-                    .filter(|&(_, ch)| ch == '#')
-                    .map(move |(x, _)| Position::new(x, y))
+            .match_indices('#')
+            .map(|(index, _)| Position {
+                x: index % scanline,
+                y: index / scanline,
             })
             .collect();
-
-        let guard: Guard = value
-            .lines()
-            .enumerate()
-            .find_map(|(y, line)| {
-                line.chars().position(|c| c == '^').map(|x| Guard {
-                    position: Position::new(x, y),
-                    direction: Direction::Up,
-                })
-            })
-            .ok_or("Guard not found")?;
 
         Ok(State {
             guard,
@@ -171,109 +184,30 @@ impl TryFrom<String> for State {
     }
 }
 
-impl Display for State {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in self.y_range.clone() {
-            for x in self.x_range.clone() {
-                let pos = Position { x, y };
 
-                // Check if the guard is at this position
-                if self.guard.position == pos {
-                    let guard_char = match self.guard.direction {
-                        Direction::Up => '^',
-                        Direction::Down => 'v',
-                        Direction::Left => '<',
-                        Direction::Right => '>',
-                    };
-                    write!(f, "{}", guard_char)?;
-                } else if self.blocks.contains(&pos) {
-                    write!(f, "#")?;
-                } else if let Some((_, _)) = self.visited.get(&(pos, Direction::Up)) {
-                    write!(f, "|")?;
-                } else if let Some((_, _)) = self.visited.get(&(pos, Direction::Down)) {
-                    write!(f, "|")?;
-                } else if let Some((_, _)) = self.visited.get(&(pos, Direction::Left)) {
-                    write!(f, "-")?;
-                } else if let Some((_, _)) = self.visited.get(&(pos, Direction::Right)) {
-                    write!(f, "-")?;
-                } else {
-                    // Empty space
-                    write!(f, ".")?;
-                }
-            }
-            writeln!(f)?; // Newline after each row
-        }
-        Ok(())
-    }
-}
 
-pub fn wait_input() {
-    let mut buffer = [0; 1];
-    std::io::Read::read_exact(&mut std::io::stdin(), &mut buffer).unwrap();
-}
+////////////////////////////////////////////////////////////////////////////////
+/// Solution
+////////////////////////////////////////////////////////////////////////////////
 
 pub fn task2(input: String) {
     let mut state = State::try_from(input).expect("Pew pew");
     let mut loops = 0;
 
-    let mut loopsSet: HashSet<(Position)> = HashSet::new();
-
-    let mut print_flag = true;
-
     /* main loop */
     loop {
-        if print_flag {
-            println!("!MAIN\n{}", state);
-            wait_input();
-        }
-
         if let Some(next_position) = state.get_next_position() {
-            println!("----------------------------------");
             let mut simulation = state.with_block(next_position);
-            println!("setting block at {:?}", next_position);
-            println!("{:?}", simulation.guard);
-
-            if next_position.x == 3 && next_position.y == 3 {
-                print_flag = true;
-            }
 
             state.make_step();
 
             /* simulation loop */
             loop {
-                // if print_flag {
-                //     println!("!SIMULATION\n{}", simulation);
-                //     wait_input();
-                // }
-
                 if None == simulation.make_step() {
-                    // if print_flag {
-                    //     println!(
-                    //         "Step to {} {} - {:?}\nVisited blocks {}",
-                    //         simulation.guard.position.x,
-                    //         simulation.guard.position.y,
-                    //         simulation.guard.direction,
-                    //         simulation.print_visited(),
-                    //     );
-                    // }
-                    // println!("Guard in - {:?}", simulation.guard);
                     if simulation.is_looping() {
-                        loopsSet.insert(next_position);
-                        println!("-------LOOP-------");
-                        println!("visited {}", simulation.print_visited());
                         loops += 1;
                     }
                     break;
-                } else {
-                    // if print_flag {
-                    //     println!(
-                    //         "Step to {} {} - {:?}\nVisited blocks {}",
-                    //         simulation.guard.position.x,
-                    //         simulation.guard.position.y,
-                    //         simulation.guard.direction,
-                    //         simulation.print_visited(),
-                    //     );
-                    // }
                 }
             }
         } else {
@@ -281,11 +215,5 @@ pub fn task2(input: String) {
         }
     }
 
-    // println!("{:?}", loopsSet);
-
     println!("Result {}", loops);
-    println!("Res 2 {}", loopsSet.len());
-
-    // (5, 3) (3, 6) (6, 8) (7, 3) (2, 3) (0, 3)
-    // println!("Initial state {:?}", state);
 }
